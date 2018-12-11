4f845575841fa3a19c7ec43adc86a3fb82165b75
fenix <a.sydorenko87@gmail.com>
2018-12-11 21:41:34 +0200

    #fix (index.js) getLang fix

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const oss = require('./os.json')
 module.exports = {
   getBrowser: (a) => browsers[a] ? browsers[a].id : 0,
   getCountry: (a) => countries[a] ? countries[a] : 0,
-  getLang: (a) => languages[a] ? languages[a] : 0,
+  getLang: (a) => languages[a] ? languages[a].id : 0,
   getOs: (a) => oss[a] ? oss[a].id : 0
 }
