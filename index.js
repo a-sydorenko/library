3af731a0b6be397064b3b873da4835308e85184b
fenix <a.sydorenko87@gmail.com>
2019-01-23 23:19:59 +0200

    #feature (countries.json, index.js, package.json) added iso2ToIso3 relations

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const browsers = require('./browsers.json')
-const countries = require('./countries.json')
+const { iso3ToInt, iso2ToIso3 } = require('./countries.json')
 const languages = require('./languages.json')
 const oss = require('./os.json')
 
 module.exports = {
   getBrowser: (a) => browsers[a] ? browsers[a].id : 0,
-  getCountry: (a) => countries[a] ? countries[a] : 0,
+  getCountry: (a) => iso3ToInt[a] ? iso3ToInt[a] : 0,
+  getCountryIso3: (a) => iso2ToIso3[a] ? iso2ToIso3[a] : '',
   getLang: (a) => languages[a] ? languages[a].id : 0,
   getOs: (a) => oss[a] ? oss[a].id : 0
 }
